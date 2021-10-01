f8efe31c45f7191593d6ff61174d7c0967c04228
unknown <fauzan4mak@gmail.com>
2021-10-01 11:21:07 +0530

    Navbar changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,31 +3,31 @@ import "./Navbar.css";
 import QueueMusicIcon from "@mui/icons-material/QueueMusic";
 import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import logo from "../../images/logo/logo.png";
 export default function Navbar() {
   return (
     <div className="sideHelper">
       <div className="sideHelperLayout">
-          <Link to='/'>
-        <img src={logo} className="logo" alt="thht" />
-        </Link>
-        <Link to='/favourites'>
+          <NavLink to='/'>
+        <img src={logo} id="musicImageAnime"  className="logo" alt="thht" />
+        </NavLink>
+        <NavLink to='/favourites'>
         <div title="favourites">
           <FavoriteIcon className="heart" style={{ fontSize: "2rem" }} />
         </div>
-        </Link>
-        <Link to="/musicList" >
+        </NavLink>
+        <NavLink to="/musicList" >
         <div title="My Library">
-          <LibraryMusicIcon className="heart" style={{ fontSize: "2rem" }} />
+          <LibraryMusicIcon className="heart" id="lib" style={{ fontSize: "2rem" }} />
         </div>
-        </Link>
-        <Link to="/musicList" >
+        </NavLink>
+        <NavLink to="/musicList" >
         <div title="My Queue">
-          <QueueMusicIcon className="heart" style={{ fontSize: "2rem" }} />
+          <QueueMusicIcon className="heart" id="lib" style={{ fontSize: "2rem" }} />
         </div>
-        </Link>
+        </NavLink>
         <div title="My Account">
           <AccountCircleIcon className="heart" style={{ fontSize: "2rem" }} />
         </div>
